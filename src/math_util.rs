f5014c58e6f72aacfeb12ee7d0a2b065a3552463
Pocket7878 <poketo7878@gmail.com>
2022-02-19 10:35:27 +0900

    fix: aim rotation.

--- src/math_util.rs
+++ src/math_util.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 
+// 0が真上で、時計まわりにしたい
 pub fn get_direction(from: &Vec3, to: &Vec3) -> f64 {
-    f64::atan2((to.x - from.x) as f64, (from.y - to.y) as f64)
+    f64::atan2((to.x - from.x) as f64, (to.y - from.y) as f64)
 }
